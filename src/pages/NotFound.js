f1f2e925895bef2fc482158aafefeb631ed6e9b9
netK-Dev <flyaxe709gmail.com>
2024-07-21 23:23:30 -1000

    Création et mise en place de la page erreur 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/NotFound.module.scss';
 
 const NotFound = () => {
-  return <h1>404 - Not Found</h1>;
+  return (
+    <div className={styles.page404}>
+      <img src="img/404.webp" alt="erreur 404" className={styles.e404} />
+      <p className={styles.txt}>
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className={styles.link}>
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
