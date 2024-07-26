61c6319d8e33dab6aaae81ed9bbb7ad3bbabb6ba
netK-Dev <flyaxe709gmail.com>
2024-07-25 18:56:10 -1000

    Création du composant StarRating

---
+++ src/components/StarRating.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import filledStar from '../asset/StarActive.svg';
+import emptyStar from '../asset/StarEmpty.svg';
+import styles from '../styles/StarRating.module.scss';
+
+const StarRating = ({ rating }) => {
+  const maxRating = 5;
+
+  // Convertir la note en nombre
+  const numericRating = parseInt(rating, 10);
+
+  // Générer les étoiles
+  const stars = [];
+  for (let i = 1; i <= maxRating; i++) {
+    stars.push(
+      <img
+        key={i}
+        src={i <= numericRating ? filledStar : emptyStar}
+        alt="Etoile de notation"
+        className={styles.star}
+      />
+    );
+  }
+
+  return <div className={styles.starRating}>{stars}</div>;
+};
+
+export default StarRating;
