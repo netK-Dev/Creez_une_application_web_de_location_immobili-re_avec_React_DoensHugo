de7f4ad14d987f6bd8a302c53c99c75b60170ede
netK-Dev <flyaxe709gmail.com>
2024-07-26 20:49:16 -1000

    correction mineur

--- src/components/ImageBanner.js
+++ src/components/ImageBanner.js
@@ -6,7 +6,6 @@ const ImageBanner = ({
   imageSrc,
   alt,
   marginTop = '38px',
-  height = '223px', // Paramètre par défaut
   opacity = 0.6, // Paramètre par défaut
   text = '', // Paramètre par défaut
 }) => {
@@ -15,7 +14,7 @@ const ImageBanner = ({
   };
 
   return (
-    <div className={styles.contImg} style={{ marginTop, height }}>
+    <div className={styles.contImg} style={{ marginTop }}>
       <img src={imageSrc} className={styles.img} alt={alt} />
       <div className={styles.overlay} style={overlayStyle}></div>
       {text && <div className={styles.text}>{text}</div>}
@@ -27,7 +26,6 @@ ImageBanner.propTypes = {
   imageSrc: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   marginTop: PropTypes.string,
-  height: PropTypes.string,
   opacity: PropTypes.number,
   text: PropTypes.string,
 };
