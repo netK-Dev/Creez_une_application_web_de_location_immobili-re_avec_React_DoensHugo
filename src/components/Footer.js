cbfb8ba04999d32e51b417bf4d037e0321c2aa32
netK-Dev <flyaxe709gmail.com>
2024-07-27 10:28:50 -1000

    correction Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ import styles from '../styles/Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <img src="img/LogoFooter.svg" alt="Logo Footer" className={styles.logo} />
+      <img
+        src="/img/LogoFooter.svg"
+        alt="Logo Footer"
+        className={styles.logo}
+      />
       <p className={styles.text}>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
