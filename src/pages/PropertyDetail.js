aba61ce8d8ad65e34d2bf03831d54547e016c6b2
netK-Dev <flyaxe709gmail.com>
2024-07-27 11:52:00 -1000

    Mise en page de ma page PropertyDetail avec l'utilisation de tout les composants nesessaire

--- src/pages/PropertyDetail.js
+++ src/pages/PropertyDetail.js
@@ -1,13 +1,21 @@
 // src/pages/PropertyDetail.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams, Navigate } from 'react-router-dom';
+import style from '../styles/PropertyDetail.module.scss';
+import Slider from '../components/Slider';
 import PropertyTag from '../components/PropertyTag';
 import StarRating from '../components/StarRating';
 import Owner from '../components/Owner';
+import Collapse from '../components/Collapse';
 
 const PropertyDetail = ({ properties }) => {
   const { id } = useParams();
   const property = properties.find((p) => p.id === id);
+
+  useEffect(() => {
+    // S'assurer que la fenêtre se positionne en haut lors du chargement du composant
+    window.scrollTo(0, 0);
+  }, [id]);
 
   if (!property) {
     // Redirige vers la page 404
@@ -15,11 +23,26 @@ const PropertyDetail = ({ properties }) => {
   }
 
   return (
-    <div>
-      <h1>{property.title}</h1>
-      <PropertyTag tags={property.tags} />
-      <StarRating rating={property.rating} />
-      <Owner owner={property.host} />
+    <div className={style.content}>
+      <Slider images={property.pictures} />
+
+      <div className={style.propertyInfo1}>
+        <div className={style.titleLocTags}>
+          <h1 className={style.title}>{property.title}</h1>
+          <p className={style.txt}>{property.location}</p>
+          <PropertyTag tags={property.tags} />
+        </div>
+
+        <div className={style.ownerAndStars}>
+          <Owner owner={property.host} />
+          <StarRating rating={property.rating} />
+        </div>
+      </div>
+
+      <div className={style.propertyInfo2}>
+        <Collapse title="Description" width="582px"></Collapse>
+        <Collapse title="Équipements" width="582px"></Collapse>
+      </div>
     </div>
   );
 };
